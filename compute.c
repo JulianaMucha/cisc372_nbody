c90883e3a8e52bf51f1dc4d11d38e1a31d701fe6
Juliana Mucha <jmucha@udel.edu>
2026-05-05 22:29:35 -0400

    First attempt (untested)

--- compute.c
+++ compute.c
@@ -3,47 +3,55 @@
 #include "vector.h"
 #include "config.h"
 
+// cudaMalloc(&d_accels, NUMENTITIES*NUMENTITIES*sizeof(vector3)); in main
+
+vector3* d_accels;
+double *d_mass;
+
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
-void compute(){
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	for (i=0;i<NUMENTITIES;i++)
-		accels[i]=&values[i*NUMENTITIES];
+__global__ void compute(){
+
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j = blockIdx.y * blockDim.y + threadIdx.y;
+
 	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
+	if (i<NUMENTITIES && j<NUMENTITIES){
+		if (i==j) {
+			FILL_VECTOR(d_accels[i*NUMENTITIES+j],0,0,0);
+		}
+		else{
+			vector3 distance;
+			for (k=0;k<3;k++) distance[k]=d_hPos[i][k]-d_hPos[j][k];
+			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+			double magnitude=sqrt(magnitude_sq);
+			double accelmag=-1*GRAV_CONSTANT*d_mass[j]/magnitude_sq;
+			FILL_VECTOR(d_accels[i*NUMENTITIES+j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
 		}
 	}
+}
+
+__global__ void update_bodies(){
+
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j, k;
+
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
+	if (i<NUMENTITIES){
 		vector3 accel_sum={0,0,0};
 		for (j=0;j<NUMENTITIES;j++){
 			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
+				accel_sum[k]+=d_accels[i*NUMENTITIES+j][k];
+			}
 		}
+
 		//compute the new velocity based on the acceleration and time interval
 		//compute the new position based on the velocity and time interval
 		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
+			d_hVel[i][k]+=accel_sum[k]*INTERVAL;
+			d_hPos[i][k]+=d_hVel[i][k]*INTERVAL;
 		}
 	}
-	free(accels);
-	free(values);
 }
