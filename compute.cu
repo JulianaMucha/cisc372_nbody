f486a950edba1f78ebc718ec3344c327ad49cc31
Juliana Mucha <jmucha@udel.edu>
2026-05-06 20:07:23 -0400

    debugging

--- compute.cu
+++ compute.cu
@@ -4,10 +4,15 @@
 #include "config.h"
 #include <cuda.h>
 
-extern vector3* d_accels;
-extern vector3* d_hPos;
-extern vector3* d_hVel;
-extern double* d_mass;
+extern __device__ vector3* d_accels;
+extern __device__ vector3* d_hPos;
+extern __device__ vector3* d_hVel;
+extern __device__ double* d_mass;
+
+//extern vector3* d_accels;
+//extern vector3* d_hPos;
+//extern vector3* d_hVel;
+//extern double* d_mass;
 
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
